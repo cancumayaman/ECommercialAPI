bdacfd611887f803c0bf9230ba9b435f7c7aec85
Can Cuma Yaman <Can.Yaman@d-teknoloji.com.tr>
2022-03-04 23:05:45 +0300

    Onion Architecture template created

---
+++ Core/ECommercialAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ECommercialAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommercialAPI.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
---
+++ Infrastructure/ECommercialAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ECommercialAPI.Application.Abstractions;
+using ECommercialAPI.Persistence.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommercialAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistemceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
